dddc90d37016b114ce226711d857fb808800c93a
lucheng <lucheng0127@outlook.com>
2024-08-14 14:51:56 +0800

    Add EndpointMgr to manage logined endpoint
    An Endpint represent a auth succeed client,
    after login, assign a ip for it according
    to the hash of login username

---
+++ pkg/endpoint/endpoint.go
@@ -0,0 +1,111 @@
+package endpoint
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/lucheng0127/vtun/pkg/protocol"
+	"github.com/vishvananda/netlink"
+)
+
+// An Endpoint represent a authed client
+type Endpoint struct {
+	Conn      *net.UDPConn
+	RAddr     *net.UDPAddr
+	User      string
+	IP        *netlink.Addr
+	LoginTime string
+	TXQueue   chan *protocol.VTPacket
+}
+
+func (ep *Endpoint) Close() error {
+	// TODO: Implement it if need
+	return nil
+}
+
+type EndpointMgr struct {
+	MLock     sync.Mutex
+	EPIPMap   map[string]string
+	EPAddrMap map[string]*Endpoint
+}
+
+func NewEPMgr() *EndpointMgr {
+	return &EndpointMgr{
+		MLock:     sync.Mutex{},
+		EPIPMap:   make(map[string]string),
+		EPAddrMap: make(map[string]*Endpoint),
+	}
+}
+
+func (mgr *EndpointMgr) GetEPAddrByIP(ip string) string {
+	addr, ok := mgr.EPIPMap[ip]
+	if ok {
+		return addr
+	}
+
+	return ""
+}
+
+func (mgr *EndpointMgr) GetEPByAddr(addr string) *Endpoint {
+	ep, ok := mgr.EPAddrMap[addr]
+	if ok {
+		return ep
+	}
+
+	return nil
+}
+
+func (mgr *EndpointMgr) NewEP(conn *net.UDPConn, raddr *net.UDPAddr, user string, ip *netlink.Addr) error {
+	ep := &Endpoint{
+		Conn:      conn,
+		RAddr:     raddr,
+		User:      user,
+		IP:        ip,
+		LoginTime: time.Now().Format("2006-01-02 15:04:05"),
+		TXQueue:   make(chan *protocol.VTPacket),
+	}
+	addrKey := raddr.String()
+	ipKey := ip.String()
+
+	// Check ip or raddr exist
+	if addr := mgr.GetEPAddrByIP(ipKey); addr != "" {
+		return fmt.Errorf("IP address %s used by Endpoint with remote address %s", ipKey, addr)
+	}
+
+	if addrEP := mgr.GetEPByAddr(addrKey); addrEP != nil {
+		return fmt.Errorf("Endpoint with remote address %s exist, ip %s", addrKey, addrEP.IP.String())
+	}
+
+	mgr.MLock.Lock()
+	mgr.EPAddrMap[addrKey] = ep
+	mgr.EPIPMap[ipKey] = addrKey
+	mgr.MLock.Unlock()
+
+	return nil
+}
+
+func (mgr *EndpointMgr) CloseEPByIP(ip string) error {
+	mgr.MLock.Lock()
+	defer mgr.MLock.Unlock()
+
+	addrKey, ok := mgr.EPIPMap[ip]
+	if !ok {
+		return nil
+	}
+
+	ep, ok := mgr.EPAddrMap[addrKey]
+	if !ok {
+		delete(mgr.EPIPMap, ip)
+		return nil
+	}
+
+	delete(mgr.EPAddrMap, addrKey)
+	delete(mgr.EPIPMap, ip)
+	if err := ep.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/endpoint/endpoint_test.go
@@ -0,0 +1,107 @@
+package endpoint
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/lucheng0127/vtun/pkg/protocol"
+	"github.com/vishvananda/netlink"
+)
+
+func TestEndpointMgr_NewEP(t *testing.T) {
+	// Old ep map infos
+	ep1IP, _ := netlink.ParseAddr("192.168.0.1/24")
+	ep1Addr, _ := net.ResolveUDPAddr("udp", "172.16.18.1:1234")
+
+	ep2IP, _ := netlink.ParseAddr("192.168.0.2/24")
+	ep2Addr, _ := net.ResolveUDPAddr("udp", "172.16.18.2:1234")
+
+	mgr := NewEPMgr()
+	mgr.EPIPMap[ep1IP.String()] = ep1Addr.String()
+	mgr.EPAddrMap[ep1Addr.String()] = &Endpoint{
+		Conn:      new(net.UDPConn),
+		RAddr:     new(net.UDPAddr),
+		User:      "ep1User",
+		IP:        ep1IP,
+		LoginTime: time.Now().Format("2006-01-02 15:04:05"),
+		TXQueue:   make(chan *protocol.VTPacket),
+	}
+	mgr.EPAddrMap[ep2Addr.String()] = &Endpoint{
+		Conn:      new(net.UDPConn),
+		RAddr:     new(net.UDPAddr),
+		User:      "ep2User",
+		IP:        ep2IP,
+		LoginTime: time.Now().Format("2006-01-02 15:04:05"),
+		TXQueue:   make(chan *protocol.VTPacket),
+	}
+
+	// Ep waiting for add
+	weConn := new(net.UDPConn)
+	weAddr, _ := net.ResolveUDPAddr("udp", "172.16.17.254:1234")
+	weUser := "weUser"
+	weIP, _ := netlink.ParseAddr("192.168.0.254/24")
+
+	type args struct {
+		conn  *net.UDPConn
+		raddr *net.UDPAddr
+		user  string
+		ip    *netlink.Addr
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				conn:  weConn,
+				raddr: weAddr,
+				user:  weUser,
+				ip:    weIP,
+			},
+			wantErr: false,
+		},
+		{
+			name: "ip used",
+			args: args{
+				conn:  new(net.UDPConn),
+				raddr: new(net.UDPAddr),
+				user:  "ep1User",
+				ip:    ep1IP,
+			},
+			wantErr: true,
+		},
+		{
+			name: "addr used",
+			args: args{
+				conn:  new(net.UDPConn),
+				raddr: ep2Addr,
+				user:  "ep2User",
+				ip:    ep2IP,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := mgr.NewEP(tt.args.conn, tt.args.raddr, tt.args.user, tt.args.ip); (err != nil) != tt.wantErr {
+				t.Errorf("EndpointMgr.NewEP() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.name == "ok" {
+				addr := mgr.GetEPAddrByIP(tt.args.ip.String())
+				if addr != tt.args.raddr.String() {
+					t.Errorf("GetEPAddrByIP() want = %v, got = %v", tt.args.raddr.String(), addr)
+				}
+
+				ep := mgr.GetEPByAddr(addr)
+				if ep.IP.String() != tt.args.ip.String() {
+					t.Errorf("Endpoint get by GetEPByAddr info not match, wantIP = %v, gotIP = %v", ep.IP.String(), tt.args.ip.String())
+				}
+			}
+		})
+	}
+}
